435496b0d2b85fa3b665019817315d2b9313f26d
Jonas Mueller <muellerj@me.com>
2015-01-14 18:18:44 +0100

    Clean up a bit

---
+++ grammar/characteristic.rb
@@ -0,0 +1,3 @@
+module Characteristic
+
+end
--- lib/a2l_parser.rb
+++ lib/a2l_parser.rb
@@ -1,21 +1,68 @@
 require "rubygems"
 require "treetop"
-require "ostruct"
-require "characteristic"
+
+require "awesome_print"
+require "pry"
 
 class A2lParser
-
   Treetop.load File.join(__dir__, "../grammar/a2l")
+  Treetop.load File.join(__dir__, "../grammar/characteristic")
 
-  def initialize(filename)
-    @filename = filename
-    @rawcontent = File.open(filename, "rb").read
-    @parser = A2lGrammarParser.new
+  require_relative "../grammar/a2l"
+  require_relative "../grammar/characteristic"
+
+  def self.parse(data)
+    a2l_parser = A2lGrammarParser.new
+    characteristic_parser = CharacteristicGrammarParser.new
+
+    ast = a2l_parser.parse(data)
+
+    if ast
+      characteristics = walk_tree(ast) { |klass| klass == A2L::Characteristic }
+      ap characteristics
+    else
+      parser.failure_reason =~ /^(Expected .+) after/m
+      puts "#{$1.gsub("\n", '$NEWLINE')}:"
+      puts data.lines.to_a[parser.failure_line - 1]
+      puts "#{'~' * (parser.failure_column - 1)}^"
+    end
   end
 
-  def characteristics
-    #[OpenStruct.new(name: "ASAM.M.SCALAR.UBYTE.IDENTICAL")]
-    @characteristics ||= @parser.parse(@rawcontent)
-  end
+  private
 
+  def self.walk_tree(root_node, ary=[], &block)
+    ary << root_node.to_array if block.call(root_node.class)
+    root_node.elements.each { |node| walk_tree(node, ary, &block) } if root_node.elements
+    ary
+  end
 end
+
+
+TESTA2L = <<-EOS.gsub(/^ {2}/, "")
+  /begin CHARACTERISTIC foo.bar.bay
+    "Scalar FW U16 and CDF20 as name"
+    VALUE
+    0x810000
+    RL.FNC.UBYTE.ROW_DIR
+    0
+    CM.IDENTICAL
+    10 200
+    EXTENDED_LIMITS 0 256
+    DISPLAY_IDENTIFIER DI.ASAM.C.SCALAR.UBYTE
+  /end CHARACTERISTIC
+
+  /begin CHARACTERISTIC K_Cnt_FIS_DAQ_IGKN_RNG_dec "0x1A040C (DTC (Hex): 0x1A040C)"
+    VALUE 0x803CBAA1 _REC_S1VAL_1_u1 0 _CNV_R_R_CM_Def_8_0_CM 0 255
+    FORMAT "%6.0"
+    DISPLAY_IDENTIFIER K_Cnt_FIS_DAQ_IGKN_RNG_dec
+    /begin ANNOTATION
+      ANNOTATION_LABEL "Calibration Note"
+      /begin ANNOTATION_TEXT
+        "0x1A040C (DTC (Hex): 0x1A040C)"
+      /end ANNOTATION_TEXT
+    /end ANNOTATION
+  /end CHARACTERISTIC
+EOS
+
+A2lParser.parse(TESTA2L)
+
